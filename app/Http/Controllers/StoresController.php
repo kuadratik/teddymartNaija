d193531221d4ff2385907a803853bf5403cc239d
“westcorperations” <richardejike0172@gmail.com>
2024-08-16 11:06:56 +0100

    mod

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Store\CreateStoreRequest;
 use App\Http\Requests\Store\UpdateStoreRequest;
 use App\Models\Store;
+use App\Support\Utils;
 use App\Utils\FileUploader;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -58,7 +59,7 @@ class StoresController extends Controller
      */
     public function uploadTempFile(Request $request)
     {
-        $path = FileUploader::uploadTemporary($request->file('images'));
+        $path = Utils::uploadTemporary($request->file('images'));
         return $this->success($path);
     }
 }
