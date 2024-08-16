c7d581cb1385fae6f35a6945b4b73af3625f669f
“westcorperations” <richardejike0172@gmail.com>
2024-08-16 14:39:45 +0100

    push

--- app/Services/Auth/UserService.php
+++ app/Services/Auth/UserService.php
@@ -2,5 +2,18 @@
 
 namespace App\Services\Auth;
 
+use App\Models\User;
+use App\Support\Utils;
 
-class UserService {}
+class UserService
+{
+
+    public function updateUserPassword($request)
+    {
+        $user = User::find(auth('api')->user()->id);
+        if (!password_verify($request['old_password'], $user->password)) {
+            return Utils::validateResp(['password' => ['The provided old password is incorrect.']]);
+        }
+        $user->update(['password' => bcrypt($request['password'])]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\FrontAuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\GeneralController;
 use App\Http\Controllers\StoresController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -41,8 +42,12 @@ Route::middleware('auth:api')->group(function () {
 
     Route::prefix('front')->group(function () {
         Route::post('logout', [FrontAuthController::class, 'logout']);
-        Route::post('file-upload', [GeneralController::class], 'TempUploadFile');
-
+        Route::post('file-upload', [GeneralController::class], 'uploadTempFile');
+        Route::prefix('user')->controller(UserController::class)->group(function () {
+            Route::get('profile', 'getUserProfile');
+            Route::patch('profile/update', 'updateUserProfile');
+            Route::patch('change-password', 'updateUserPassword');
+        });
     });
 });
 
