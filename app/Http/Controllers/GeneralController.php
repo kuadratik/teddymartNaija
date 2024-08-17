2ae9e054260195cf29d21d2ffb393375c3e8c606
“westcorperations” <richardejike0172@gmail.com>
2024-08-17 06:41:13 +0100

    uöoadfile psr fix and utill support update etc

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Support\Utils;
 use App\Http\Requests\Store\UploadTempFileRequest;
+use Illuminate\Support\Facades\Validator;
 
 class GeneralController extends Controller
 {
@@ -15,5 +16,23 @@ class GeneralController extends Controller
     {
         $path = Utils::uploadTemporary($request->file('images'));
         return $this->success($path);
+    }
+
+
+    /**
+     * delete temporary file paths
+     */
+    public function deleteTempFiles(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'paths' => 'required|array',
+            'paths.*' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return Utils::validateResp($validator->errors());
+        }
+        Utils::deleteTemporaryFiles($request->input('paths'));
+        return $this->success();
     }
 }
