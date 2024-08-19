ed85955b7c56573b9bab1ffcede08e055606108a
“westcorperations” <richardejike0172@gmail.com>
2024-08-19 13:47:13 +0100

    fix email unique bug

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -24,7 +24,7 @@ class UpdateUserRequest extends FormRequest
         return [
             'first_name' => ['nullable', 'string'],
             'last_name' => ['nullable', 'string'],
-            'email' => ['nullable', 'email'],
+            'email' => ['nullable', 'email', 'unique:users,email,' . auth('api')->id()],
         ];
     }
 }
