19564cb29f322c73cb2f48622853919bb0127f51
“westcorperations” <richardejike0172@gmail.com>
2024-08-19 22:29:27 +0100

    push country endpoint

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Store\UploadTempFileRequest;
 use App\Models\Category;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class GeneralController extends Controller
 {
@@ -55,5 +56,48 @@ class GeneralController extends Controller
         });
 
         return $this->success($categories);
+    }
+
+
+
+    /**
+     * Get all countries
+     */
+    public function countries(Request $request)
+    {
+        $cacheKey = 'countries_' . $request->search ?? 'all';
+
+        $countries = Cache::remember($cacheKey, now()->addMinutes(60), function () use ($request) {
+            return DB::table('countries')
+            ->select('id', 'name', 'emoji', 'currency_code', 'phonecode')
+            ->where(function ($query) use ($request) {
+                if ($request->has('search')) {
+                    $query->where('name', 'LIKE', "%$request->search%")
+                    ->orWhere('phonecode', 'LIKE', "%$request->search%");
+                }
+            })->get();
+        });
+
+        return $this->success($countries);
+    }
+
+    /**
+     * Get country divisions
+     */
+    public function countryDivision(Request $request)
+    {
+        $cacheKey = 'country_divisions_' . $request->country . '_' . $request->search ?? 'all';
+
+        $divisions = Cache::remember($cacheKey, now()->addMinutes(60), function () use ($request) {
+            return DB::table('states')
+            ->where('country_id', $request->country)
+                ->where(function ($query) use ($request) {
+                    if ($request->has('search')) {
+                        $query->where('name', 'LIKE', "%$request->search%");
+                    }
+                })->get();
+        });
+
+        return $this->success($divisions);
     }
 }
