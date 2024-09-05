e2bf1034c26bd215f100fbb66a6a136136d76172
“westcorperations” <richardejike0172@gmail.com>
2024-09-05 18:09:39 +0100

    mail config header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,20 @@
 @props(['url'])
-<tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
-</tr>
+<div style="width: 100%; background-color: #f6f6f6; padding: 20px; display: flex; justify-content: space-between; align-items: center;">
+    <div style="flex: 1;">
+        <a href="{{ url('/') }}" style="display: inline-block;">
+            <img src="{{ asset('images/logo.png') }}" alt="Eki Logo" style="height: 50px;">
+        </a>
+    </div>
+    <div style="flex: 1; text-align: right;">
+        <a href="https://twitter.com" style="margin: 0 10px;">
+            <img src="{{ asset('images/SocialIconTwitter.png') }}" alt="Twitter" style="height: 24px;">
+        </a>
+        <a href="https://facebook.com" style="margin: 0 10px;">
+            <img src="{{ asset('pimages/SocialIconFB.png') }}" alt="Facebook" style="height: 24px;">
+        </a>
+        <a href="https://instagram.com" style="margin: 0 10px;">
+            <img src="{{ asset('images/SocialIconIG.png') }}" alt="Instagram" style="height: 24px;">
+        </a>
+    </div>
+</div>
+
