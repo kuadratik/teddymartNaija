88592fe79962ac1dfb843299dcbd7a340d6b2ceb
“westcorperations” <richardejike0172@gmail.com>
2024-09-20 21:42:35 +0100

    add  index

--- database/migrations/2024_09_20_141943_add_type_column_to_orders_table.php
+++ database/migrations/2024_09_20_141943_add_type_column_to_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->string('type')->nullable()->after('store_id');
+            $table->string('type')->nullable()->after('store_id')->index();
         });
     }
 
