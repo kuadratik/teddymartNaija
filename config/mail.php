750374ee620b5d02f8edc6478334e9118d886781
“westcorperations” <richardejike0172@gmail.com>
2024-09-24 12:12:54 +0100

    new: contact us

--- config/mail.php
+++ config/mail.php
@@ -49,6 +49,9 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'inquiry_email' => env('INQUIRY_EMAIL', 'Inquiries@ekimarket.com'),
+
+
         'ses' => [
             'transport' => 'ses',
         ],
