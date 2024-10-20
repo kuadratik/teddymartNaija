84c89ff9e25aeed3a97ac450b78695d9400b4fa5
“westcorperations” <richardejike0172@gmail.com>
2024-10-20 18:47:32 +0100

    add currency to store

--- app/Enums/CurrencyType.php
+++ app/Enums/CurrencyType.php
@@ -4,9 +4,10 @@ namespace App\Enums;
 
 enum CurrencyType: string
 {
-   case AUD = 'AUD';
-   case CAD = 'CAD';
-   case USD = 'USD';
-   case GBP = 'GBP';
-   case EUR = 'EUR';
+    case AUD = 'AUD';
+    case CAD = 'CAD';
+    case USD = 'USD';
+    case GBP = 'GBP';
+    case EUR = 'EUR';
+    case NGN = 'NGN';
 }
