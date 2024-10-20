615ecb7eddb4d06882453ca66fd6b547da6cc888
“westcorperations” <richardejike0172@gmail.com>
2024-10-20 19:17:40 +0100

    alter add_column_currency_to_stores_table.php

--- database/migrations/2024_10_20_173017_add_column_currency_to_stores_table.php
+++ database/migrations/2024_10_20_173017_add_column_currency_to_stores_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\CurrencyType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('stores', function (Blueprint $table) {
-            $table->string('currency')->nullable()->after('postal_code')->index();
+            $table->string('currency')->default(CurrencyType::USD)->after('postal_code')->index();
         });
     }
 
