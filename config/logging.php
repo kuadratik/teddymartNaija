ab0ee5ef2abda5363c84eb1de8050ce73a9d8ad8
logic <afuwapesunday12@gmail.com>
2024-11-07 10:06:48 +0100

    change file permission to read write for everyone

--- config/logging.php
+++ config/logging.php
@@ -71,7 +71,7 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => env('LOG_DAILY_DAYS', 14),
             'replace_placeholders' => true,
-            'permission' => 0664,
+            'permission' => 0666,
         ],
 
         'slack' => [
