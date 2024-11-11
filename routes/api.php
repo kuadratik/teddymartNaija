633d2057d1a9cd3c4bb4c8949fbc5a88df925233
“westcorperations” <richardejike0172@gmail.com>
2024-11-11 15:59:51 +0100

    user promoted store

--- routes/api.php
+++ routes/api.php
@@ -109,7 +109,8 @@ Route::middleware('auth:sanctum')->group(function () {
         });
 
         Route::prefix('advert')->group(function () {
-            Route::post('/promote', [AdvertListingController::class, 'postStoreAdvert']);
+            Route::post('promote', [AdvertListingController::class, 'postStoreAdvert']);
+            Route::get('store/promoted-store', [AdvertListingController::class, 'getUserPromotedStore']);
         });
     });
 
