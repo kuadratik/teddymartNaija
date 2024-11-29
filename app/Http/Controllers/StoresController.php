bb87d7db080c910923f2db2f235df0d159235648
westlove <richardEjike0172@gmail.com>
2024-11-29 10:21:34 +0100

    fix

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -203,12 +203,12 @@ class StoresController extends Controller
         $user = $request->user();
         $status = $request->query('order_status');
         $orders = $store->orders()->where('user_id', $user->id)
-            ->where(['type', ListingType::PRODUCT->value])
+            ->where('type', ListingType::PRODUCT->value)
             ->when($status, fn($query) => $query->where('status', $status))
             ->orderBy('created_at', 'desc')
             ->with('orderDetails')
-            ->paginate(20)
-            ->groupBy('order_number');
+        ->paginate(20);
+
 
 
         return $this->success($orders);
@@ -225,5 +225,4 @@ class StoresController extends Controller
 
         return $this->success();
     }
-
 }
