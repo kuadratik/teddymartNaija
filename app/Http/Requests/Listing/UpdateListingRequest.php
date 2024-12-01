7bd8163e145a5f798b2e5064ef41d31b8ad45475
westlove <richardEjike0172@gmail.com>
2024-12-01 16:02:52 +0100

    add unit to size mod fix

--- app/Http/Requests/Listing/UpdateListingRequest.php
+++ app/Http/Requests/Listing/UpdateListingRequest.php
@@ -37,15 +37,16 @@ class UpdateListingRequest extends FormRequest
             'sku' => ['sometimes', 'string', 'max:50'],
             'is_draft' => ['sometimes', 'boolean'],
 
-            'attributes.measurement' => ['nullable', 'string'],
-            'attributes.product_model' => ['nullable', 'string'],
+            'attributes.measurement' => ['nullable', 'array'],
+            'attributes.measurement.*' => ['nullable', 'array'],
+            'attributes.measurement.*.value' => ['nullable', 'numeric'],
+            'attributes.measurement.*.unit' => ['nullable', 'string'],
+            'attributes.product_model' => ['sometimes', 'string'],
             'attributes.brand' => ['sometimes', 'string'],
             'attributes.material' => ['sometimes', 'string'],
             'attributes.color' => ['sometimes', 'string'],
             'attributes.size' => ['sometimes', 'array'],
-            'attributes.size.*' => ['required', 'array'],
-            'attributes.size.*.size' => ['required', 'string'],
-            'attributes.size.*.unit' => ['required', 'string'],
+            'attributes.size.*' => ['required', 'string'],
             'attributes.tags' => ['sometimes', 'array'],
             'attributes.tags.*' => ['sometimes', 'string'],
             'attributes.size_chart_html' => ['sometimes', 'string'],
@@ -57,10 +58,12 @@ class UpdateListingRequest extends FormRequest
             'variants.*.price' => ['sometimes', 'numeric', 'min:0'],
             'variants.*.discount' => ['sometimes', 'numeric', 'min:0', 'max:100'],
             'variants.*.size' => ['sometimes', 'array'],
-            'variants.*.size.*.size' => ['required', 'string'],
-            'variants.*.size.*.unit' => ['required', 'string'],
+            'variants.*.size.*' => ['required', 'string'],
             'variants.*.color' => ['sometimes', 'string'],
-            'variants.*.measurement' => ['sometimes', 'string'],
+            'variants.*.measurement' => ['nullable', 'array'],
+            'variants.*.measurement.*' => ['nullable', 'array'],
+            'variants.*.measurement.*.value' => ['nullable', 'numeric'],
+            'variants.*.measurement.*.value' => ['nullable', 'string'],
             'variants.*.discount_start_date' => ['sometimes', 'required_with:variants.*.discount', 'date'],
             'variants.*.discount_end_date' => ['sometimes', 'required_with:variants.*.discount', 'date', 'after:variants.*.discount_start_date'],
             'variants.*.images.*' => ['sometimes', 'string'],
@@ -97,10 +100,11 @@ class UpdateListingRequest extends FormRequest
      */
     public function listingAttributeAttributes()
     {
-        return collect($this->safe()['attributes'] ?? [])->except(['size_chart_image', 'size', 'tags'])->merge([
+        return collect($this->safe()['attributes'] ?? [])->except(['size_chart_image', 'size', 'tags', 'measurement'])->merge([
             'size_chart_image' => $this->images([$this->safe()['attributes']['size_chart_image'] ?? null])[0] ?? null,
             'size' => json_encode($this->safe()['attributes']['size'] ?? []),
             'tags' => json_encode($this->safe()['attributes']['tags'] ?? []),
+            'measurement' => json_encode($this->safe()['attributes']['measurement'] ?? [])
         ])->toArray();
     }
 
@@ -110,9 +114,10 @@ class UpdateListingRequest extends FormRequest
     public function variantsAttributes()
     {
         return collect($this->safe()['variants'] ?? [])->map(function ($variant) {
-            return collect($variant)->except(['images', 'size'])->merge([
-                'images' => $this->images($variant['images'] ?? []),
-                'size' => json_encode($variant['size'] ?? [])
+            return collect($variant)->except(['images', 'size', 'measurement'])->merge([
+                'images' => $this->images($variant['images'] ?? []),'size' => json_encode($variant['size'] ?? []),
+                'measurement' => json_encode($variant['measurement'] ?? [])
+
             ])->toArray();
         })->toArray();
     }
