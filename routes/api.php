1df64b5120baa7cf75b0fff3787d393d6f78b56c
logic <afuwapesunday12@gmail.com>
2024-12-10 06:26:59 +0100

    password check

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('front')->group(function () {
     Route::post('login', [FrontAuthController::class, 'login']);
+    Route::post('auth-confirmation', [FrontAuthController::class, 'authConfirmation'])->middleware(['auth:sanctum']);
     Route::post('google-auth', [FrontAuthController::class, 'googleAuth']);
     Route::post('register', [FrontAuthController::class, 'register']);
     Route::post('register/verify', [FrontAuthController::class, 'verifyEmailOtp']);
