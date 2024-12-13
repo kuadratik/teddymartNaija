cfcbc129ecfae34abf8e18657c3084e31c1e128e
westlove <richardEjike0172@gmail.com>
2024-12-13 12:55:01 +0100

    fix

--- app/Notifications/Listing/PromotionExpiredNotification.php
+++ app/Notifications/Listing/PromotionExpiredNotification.php
@@ -42,7 +42,7 @@ class PromotionExpiredNotification extends Notification implements ShouldQueue
         return (new MailMessage)
             ->priority(1)
             ->subject('Oh no! Your Store Promotion has Expired - Time to Renew!')
-            ->greeting("Dear {$notifiable->name},")
+            ->greeting("Dear {$notifiable->first_name},")
             ->line('We wanted to let you know that your store promotion on myEKI has now expired. We hope it helped drive great traffic and visibility to your business! Your listing will now appear without the promotional boost, but you can easily renew it to continue attracting more customers.')
             ->line(new HtmlString($line1))
             ->line('If you have any questions or need assistance, do not hesitate to reach out. We are here to support your success!')
