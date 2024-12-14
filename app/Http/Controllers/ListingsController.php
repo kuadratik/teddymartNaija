668335da948b1c268cc2efc76f35619096b65be5
westlove <richardEjike0172@gmail.com>
2024-12-14 07:46:24 +0100

    fix

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -9,6 +9,7 @@ use App\Jobs\RecordCategoryInteractions;
 use App\Models\Listing;
 use App\Models\ListingRating;
 use App\Models\Store;
+use App\Services\Store\StoreService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -16,7 +17,7 @@ class ListingsController extends Controller
 {
     protected $user;
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, protected StoreService $storeService)
     {
         $this->user = $request->user();
     }
@@ -27,26 +28,7 @@ class ListingsController extends Controller
     public function getUserStoreListings(Request $request, Store $userStore)
     {
         abort_if($userStore->user_id !== $this->user->id, 403, "Unauthorized");
-
-        $userStoreListings = $userStore->listings()
-            ->latest()
-            ->byType($request->listingType)
-            ->when($request->filled('search'), function ($query) use ($request) {
-                $query->where(function ($q) use ($request) {
-                    $q->where('name', 'like', '%' . $request->search . '%')
-                        ->orWhere('description', 'like', '%' . $request->search . '%')
-                        ->orWhere('slug', 'like', '%' . $request->search . '%');
-                });
-            })
-            ->when($request->filled('availability'), function ($query) use ($request) {
-                $query->availability($request->availability);
-            })
-            ->when($request->filled('is_draft'), function ($query) use ($request) {
-                $query->isDraft($request->query('is_draft'));
-            })
-            ->with('ratings')
-            ->paginate();
-
+        $userStoreListings = $this->storeService->getUserStoreListings($request, $userStore);
         return $this->success($userStoreListings);
     }
 
