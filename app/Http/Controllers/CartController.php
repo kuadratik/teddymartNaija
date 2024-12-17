5659253f3c571c5157ad04b117e7ddfb10fbf475
yungfarz <ajanifarouq@gmail.com>
2024-12-17 16:21:00 +0100

    internation payout , order history ordering by created_at and restoration of process payout doc

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -118,7 +118,8 @@ class CartController extends Controller
      */
     public function getOrderHistory(Request $request)
     {
-        $userOrders = Order::where('user_id', $request->user()->id)->with('store:id,name', 'orderDetails')->get();
+        $userOrders = Order::where('user_id', $request->user()->id)->with('store:id,name', 'orderDetails')
+            ->latest()->get();
         $userOrderHistory = $userOrders->groupBy('order_number');
 
         return $this->success($userOrderHistory);
