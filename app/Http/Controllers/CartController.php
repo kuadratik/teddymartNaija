a16ad34c6f2133bbce0b88a1fc6414df312ac101
westlove <richardEjike0172@gmail.com>
2025-02-13 17:57:13 +0100

    Implement AddToCartRequest for validation and enhance cart functionality to support product variants

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Enums\ListingType;
 use App\Enums\OrderStatusEnum;
+use App\Http\Requests\Cart\AddToCartRequest;
 use App\Http\Requests\Cart\StoreOrderRequest;
 use App\Http\Requests\Cart\StoreShippingAddressRequest;
 use App\Models\Cart;
@@ -23,10 +24,12 @@ class CartController extends Controller
         //
     }
 
-    public function addToCart(Request $request, Listing $product)
+    /**
+     * Adds a product to the cart.
+     */
+    public function addToCart(AddToCartRequest $request, Listing $product)
     {
         $data = $this->cartService->addToCart($request, $product);
-
         return $this->success($data);
     }
 
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -20,7 +20,7 @@ class Cart extends Model
     public function products()
     {
         return $this->belongsToMany(Listing::class, 'cart_listing')
-            ->withPivot('quantity')
+            ->withPivot('quantity', 'is_variant', 'listing_variant_id')
             ->withTimestamps();
     }
 }
