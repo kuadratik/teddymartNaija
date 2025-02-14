6f582208d7df4a9b2b332fcedf244a1abf958242
westlove <richardEjike0172@gmail.com>
2025-02-14 20:18:38 +0100

    Fix indentation in the down method of the soft delete migration for advert listings

--- database/migrations/2025_02_14_180807_add_softdelete_to_advert_listings_table.php
+++ database/migrations/2025_02_14_180807_add_softdelete_to_advert_listings_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('advert_listings', function (Blueprint $table) {
-        $table->dropSoftDeletes();
+            $table->dropSoftDeletes();
         });
     }
 };
