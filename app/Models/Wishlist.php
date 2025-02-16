0fcce9d752b828f886f37969b336527aaf8a475a
westlove <richardEjike0172@gmail.com>
2025-02-16 21:28:49 +0100

    Refactor wishlist model and migrations to support polymorphic relationships and update unique constraints

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\WishlistType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,26 @@ class Wishlist extends Model
     use HasFactory;
 
     protected $table = 'wishlists';
+    protected $fillable = ['user_id', 'wishlistable_id', 'wishlistable_type'];
+
+    public function wishlistable()
+    {
+        return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeProduct($query)
+    {
+        return $query->where('wishlistable_type', WishlistType::PRODUCT);
+    }
+
+
+
+
+
+
 }
