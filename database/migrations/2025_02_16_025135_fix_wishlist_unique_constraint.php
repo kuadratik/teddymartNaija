b756553b173697eeb9affe79377fd769e4e720e5
westlove <richardEjike0172@gmail.com>
2025-02-17 02:03:25 +0100

    Fix wishlist migration to retain foreign key constraint for listing_id and adjust unique constraints

--- database/migrations/2025_02_16_025135_fix_wishlist_unique_constraint.php
+++ database/migrations/2025_02_16_025135_fix_wishlist_unique_constraint.php
@@ -12,8 +12,8 @@ return new class extends Migration
         Schema::table('wishlists', function (Blueprint $table) {
             // $table->dropForeign('wishlists_user_id_foreign');
             // $table->dropForeign('wishlists_listing_id_foreign');
-            // $table->dropUnique('wishlists_user_id_listing_id_unique');
-            // $table->dropIndex('wishlists_listing_id_foreign');
+            $table->dropUnique('wishlists_user_id_listing_id_unique');
+            $table->dropIndex('wishlists_listing_id_foreign');
             $table->dropColumn('listing_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unique(['user_id', 'wishlistable_id', 'wishlistable_type']);
