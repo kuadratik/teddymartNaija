6378ca65ecd17a5a363e7537a59d3635b4312445
westcorperations <richardEjike0172@gmail.com>
2025-03-21 19:30:07 +0100

    Add command to check and reset expired discounts

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,10 @@
 <?php
 
 use App\Console\Commands\CacheStores;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(CacheStores::class)->everyThreeMinutes();
 
 Schedule::command('advert:check-expired-adverts')->everyFifteenMinutes();
 Schedule::command('advert:check-expired-promotions')->everyFifteenMinutes();
-
+Schedule::command('discounts:check-expired')->everyMinute();
