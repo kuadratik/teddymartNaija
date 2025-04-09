bd16179a71a7e32e7b81163a3ef5140956e4f2a2
westcorperations <richardEjike0172@gmail.com>
2025-04-09 08:22:33 +0100

    Add 'shipped_at' field to Order model for tracking shipment date

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -38,6 +38,7 @@ class Order extends Model
         'store_shipping_method_id',
         'shipping_address_id',
         'delivered_notification_count',
+        'shipped_at',
     ];
 
 
@@ -45,7 +46,6 @@ class Order extends Model
         'total_amount' => 'decimal:2',
         'created_at' => 'datetime:Y-m-d H:i:s',
         'updated_at' => 'datetime:Y-m-d H:i:s',
-        'shipped_at' => 'datetime:Y-m-d H:i:s',
         'shipped_at' => 'datetime:Y-m-d H:i:s',
         'delivered_notification_count' => 'integer',
     ];
