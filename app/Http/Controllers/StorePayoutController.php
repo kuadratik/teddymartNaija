750ea508edf6ed524d8ff2ab5f24d694f985f5e6
westcorperations <richardEjike0172@gmail.com>
2025-05-01 02:23:10 +0100

    Implement OTP verification for payout details: add send and verify OTP methods, corresponding request validation, and update API routes.

--- app/Http/Controllers/StorePayoutController.php
+++ app/Http/Controllers/StorePayoutController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers;
 use App\Enums\OrderStatusEnum;
 use App\Enums\PaymentGatewayEnum;
 use App\Http\Requests\Store\SavePayoutDetailRequest;
+use App\Http\Requests\Store\VerifyOtpRequest;
 use App\Models\Order;
 use App\Models\Store;
 use App\Models\StorePayoutDetail;
+use App\Models\Otp;
+use App\Rules\ValidOtp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Notifications\SendOtpNotification;
 
 class StorePayoutController extends Controller
 {
@@ -120,5 +125,41 @@ class StorePayoutController extends Controller
     {
         $storePayoutDetail->delete();
         return $this->success();
+    }
+
+
+    /**
+     * Sends OTP to user's email for payout details verification
+     */
+    public function sendPayoutDetailsOtp(Request $request, Store $userStore)
+    {
+        abort_if($userStore->user_id !== $request->user()->id, 403);
+
+        $otp = random_int(100000, 999999);
+        Otp::updateOrCreate(
+            ['email' => $request->user()->email],
+            [
+                'otp' => Hash::make($otp),
+                'expires_at' => now()->addMinutes(5),
+                'is_used' => false,
+            ]
+        );
+
+        $request->user()->notify(new SendOtpNotification($otp));
+
+        return $this->success([], 'OTP sent successfully.');
+    }
+
+    /**
+     * Verify OTP for store payout details
+     */
+    public function verifyPayoutDetailsOtp(VerifyOtpRequest $request, Store $userStore)
+    {
+        abort_if($userStore->user_id !== $request->user()->id, 403);
+
+        $otpRecord = Otp::where('email', $request->user()->email)->first();
+        $otpRecord->update(['is_used' => true]);
+
+        return $this->success([], 'OTP verified successfully.');
     }
 }
