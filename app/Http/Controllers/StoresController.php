dc6431190c8dbdff03f618278ddef5e6aa1be796
westcorperations <richardEjike0172@gmail.com>
2025-10-09 08:18:32 +0100

    weigg fix

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -248,7 +248,7 @@ class StoresController extends Controller
     public function showStoreOrder(Store $store, Order $order)
     {
         abort_if($store->id !== $order->store_id, 403, 'Unauthorized');
-        $order->load(['orderDetails', 'orderDetails.variant', 'store', 'customer', 'payments', 'shippingAddress', 'shippingMethod']);
+        $order->load(['orderDetails.listing:id,name,price,weight', 'orderDetails.variant', 'store', 'customer', 'payments', 'shippingAddress', 'shippingMethod']);
         return $this->success($order);
     }
 
