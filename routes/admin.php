4fd91cedfce5afa4c44192692e6ff24f1420220c
westcorperations <richardEjike0172@gmail.com>
2025-10-30 11:26:24 +0100

    Add BrandHistoryNoteController and API routes for managing history notes

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminStoreController;
 use App\Http\Controllers\Admin\BrandCategoriesController;
 use App\Http\Controllers\Admin\BrandController;
+use App\Http\Controllers\Admin\BrandHistoryNoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
@@ -11,15 +12,13 @@ Route::prefix('admin')->group(function () {
 
     Route::middleware(['auth:sanctum', 'admin'])->group(function () {
         Route::post('logout', [AdminAuthController::class, 'logout']);
-        
+
         Route::patch('stores/{store}/deactivate', [AdminStoreController::class, 'deactivate']);
         Route::patch('stores/{store}/activate', [AdminStoreController::class, 'activate']);
 
         Route::prefix('brands')->group(function () {
             Route::get('/', [BrandController::class, 'index']);
             Route::post('/', [BrandController::class, 'store']);
-            Route::get('/history', [BrandController::class, 'history']);
-            Route::patch('/history/{historyId}/note', [BrandController::class, 'updateHistoryNote']);
             Route::post('/slug-recommendation', [BrandController::class, 'slugRecommendation']);
             Route::get('/{brand}', [BrandController::class, 'show']);
             Route::put('/{brand}', [BrandController::class, 'update']);
@@ -27,11 +26,17 @@ Route::prefix('admin')->group(function () {
             Route::patch('/{brand}/archive', [BrandController::class, 'archive']);
             Route::patch('/{brand}/unarchive', [BrandController::class, 'unarchive']);
 
-
-
             Route::prefix('categories')->group(function () {
                 Route::get('all', [BrandCategoriesController::class, 'index']);
                 Route::post('/', [BrandCategoriesController::class, 'store']);
+            });
+
+            Route::prefix('history-notes')->group(function () {
+                Route::get('/all', [BrandHistoryNoteController::class, 'list']);
+                Route::post('/', [BrandHistoryNoteController::class, 'create']);
+                Route::get('/{id}', [BrandHistoryNoteController::class, 'show']);
+                Route::put('/{id}', [BrandHistoryNoteController::class, 'update']);
+                Route::delete('/{id}', [BrandHistoryNoteController::class, 'delete']);
             });
         });
     });
